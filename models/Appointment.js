92f6b7fcc634bbab9dbd9d6cb6a88768f03c44e7
Aser <ajhubero16@gmail.com>
2023-03-21 22:57:29 +0800

    Added the Appointment Model and modified .gitignore to hide .env files in github

---
+++ models/Appointment.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const dbUrl = process.env.db_url;
+
+mongoose
+  .connect(dbUrl)
+  .then(() => console.log("Connected to Database - Appointment"))
+  .catch((e) => console.log("Error on Appointment ", e));
+
+const appointment_schema = new mongoose.Schema({
+  doctor: {
+    type: new mongoose.Schema({
+      full_name: String,
+      profile_pic: String,
+      specialty: String,
+    }),
+  },
+
+  patient: {
+    type: new mongoose.Schema({
+      full_name: String,
+      profile_pic: String,
+    }),
+  },
+
+  amount: {
+    type: String,
+  },
+
+  date: {
+    type: String,
+    required: true,
+  },
+
+  prescription: {
+    type: String,
+  },
+
+  status: {
+    type: String,
+  },
+});
+
+const Appointment = mongoose.model("Appointment", appointment_schema);
+
+modules.export = Appointment;
